5e234435e8eaa545aace4b4e09742c6855fd550d
PR <ranipriyanka713@gmail.com>
2017-11-15 18:17:06 +0530

    footer component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from '../components/Header.js';
 import Content from '../components/Content.js';
+import Footer from '../components/Footer.js';
+
 
 
 export default class App extends React.Component {
    render() {
       return (
          <div>
-            <Header/>
-            <Content/>
+            <Header />
+            <Content />
+            <Footer />
          </div>
       );
    }
